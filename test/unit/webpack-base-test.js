75ba4d8bbc9ae66bea2c8e0285cfc4bc6778253c
jinxiaoliu <jinxiaoliu@tencent.com>
2019-12-15 17:34:46 +0800

    test: mocha test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,10 +4,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base');
   console.log(baseConfig);
   it('entry', () => {
-    assert.equal(baseConfig.entry.index,
-      '/Users/jinxiaoliu/web/liujinxiao/webpackTest/builder-webpack/test/smoke/template/src/index/index.js');
-
-    assert.equal(baseConfig.entry.search,
-      '/Users/jinxiaoliu/web/liujinxiao/webpackTest/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
